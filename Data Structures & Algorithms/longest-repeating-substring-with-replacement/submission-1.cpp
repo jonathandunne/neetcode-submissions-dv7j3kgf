b6fb5bb567fc5c148fc4c351d10f22d56118228e
Jonathan Dunne <jonnied@umich.edu>
2026-04-14 09:07:48 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int longest = 0;
+        int count[26] = {0};
+        int greatest = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < s.length()){
+            count[s[right] - 'A']++;
+            greatest = max(greatest, count[s[right] - 'A']);
+            
+            while ((right - left + 1) - greatest > k) {
+                count[s[left] - 'A']--;
+                left++;
+                greatest = 0;
+                for (int i = 0; i < 26; i++) {
+                    greatest = max(greatest, count[i]);
+                }
+            }
+
+            longest = max(longest, (right - left + 1));
+            right++;
+        }
+
+
+
+        return longest;
+    }
+};
